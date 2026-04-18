6172ee0aad490102796ba310fa0cba167b0d16bb
Manoah <manoahgoognews@gmail.com>
2026-04-18 23:15:35 +0100

    Add redux-persist to maintain cart and order state across page reloads

--- ecommerce-app/src/app/store.js
+++ ecommerce-app/src/app/store.js
@@ -1,8 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
