5643a44c183e6ec720208b9694b78ebb350ebb47
Manoah <manoahgoognews@gmail.com>
2026-04-24 15:59:01 +0100

    Reset routing to separate auth pages from main layout and prevent navbar rendering on login/register

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,29 +1,88 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import ProductDetails from "./pages/ProductDetails";
 import Wishlist from "./pages/Wishlist";
-import Layout from "./components/Layout";
+import Checkout from "./pages/Checkout";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Navbar />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Layout>
+      <Routes>
+
+        {/* ✅ AUTH PAGES (NO NAVBAR) */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* ✅ MAIN APP PAGES (WITH NAVBAR) */}
+        <Route
+          path="/home"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+
+        <Route
+          path="/cart"
+          element={
+            <>
+              <Navbar />
+              <Cart />
+            </>
+          }
+        />
+
+        <Route
+          path="/orders"
+          element={
+            <>
+              <Navbar />
+              <Orders />
+            </>
+          }
+        />
+
+        <Route
+          path="/wishlist"
+          element={
+            <>
+              <Navbar />
+              <Wishlist />
+            </>
+          }
+        />
+
+        <Route
+          path="/checkout"
+          element={
+            <>
+              <Navbar />
+              <Checkout />
+            </>
+          }
+        />
+
+        <Route
+          path="/product/:id"
+          element={
+            <>
+              <Navbar />
+              <ProductDetails />
+            </>
+          }
+        />
+
+      </Routes>
     </BrowserRouter>
   );
 }
